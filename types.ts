71785ef120bacd040a17cc7b616985be10b33f8c
Vivek Patil <163623130+Vivek130405@users.noreply.github.com>
2026-02-16 00:21:47 +0530

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,16 @@
+
+export interface Particle {
+  id: number;
+  x: number;
+  y: number;
+  size: number;
+  duration: number;
+  delay: number;
+}
+
+export interface FloatingHeart {
+  id: number;
+  x: number;
+  size: number;
+  duration: number;
+}
